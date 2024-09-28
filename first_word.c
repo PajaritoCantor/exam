3a085f116248ad1897d281bcb0648fa1570a8d63
PajaritoCantor <juandrod@ucm.es>
2024-09-28 22:06:48 +0200

    using isspace

--- first_word.c
+++ first_word.c
@@ -3,40 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   first_word.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jurodrig <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 20:24:38 by jurodrig          #+#    #+#             */
-/*   Updated: 2024/09/19 00:14:50 by jurodrig         ###   ########.fr       */
+/*   Updated: 2024/09/28 22:03:25 by jurodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-Escriba un programa que tome una cadena y muestre su primera palabra, seguida de una
-nueva línea.
+Escriba un programa que tome una cadena y muestre su primera palabra, 
+seguida de una nueva línea.
 
-Una palabra es una sección de una cadena delimitada por espacios/tablillas o por el inicio/final de
-la cadena.
+Una palabra es una sección de una cadena delimitada por espacios/tablillas o por el inicio/final de la cadena.
 
 Si el número de parámetros no es 1, o si no hay palabras, simplemente muestra
 una nueva línea.
 */
 
 #include <unistd.h>
+#include <stdio.h>
 
-int	main(int ac, char **av)
+int ft_isspace(int c) 
 {
-	int	i;
+    return (c == 32 || c == 9);
+}
 
-	i = 0;
-	if (ac == 2)
-	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
-			i++;
-		while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
+int main(int ac, char **av) 
+{
+    int i = 0;
+
+    if (ac == 2) {
+        // Saltar espacios y tabulaciones iniciales
+        while (ft_isspace(av[1][i])) 
 		{
-			write(1, &av[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
+            i++;
+        }
+
+        // Imprimir la primera palabra
+        while (av[1][i] != '\0' && !ft_isspace(av[1][i])) 
+		{
+            write(1, &av[1][i], 1);
+            i++;
+        }
+    }
+
+    // Nueva línea al final
+    write(1, "\n", 1);
+    return (0);
 }
