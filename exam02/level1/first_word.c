1f13ebf8f8e139d535a8c0493dda5bdeb9a131d9
PajaritoCantor <juandrod@ucm.es>
2024-12-06 12:45:57 +0100

    updated

--- exam02/level1/first_word.c
+++ exam02/level1/first_word.c
@@ -5,46 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/18 20:24:38 by jurodrig          #+#    #+#             */
-/*   Updated: 2024/11/08 10:27:29 by jurodrig         ###   ########.fr       */
+/*   Created: 2024/12/06 11:37:13 by jurodrig          #+#    #+#             */
+/*   Updated: 2024/12/06 12:44:49 by jurodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-Escriba un programa que tome una cadena y muestre su primera palabra,
-seguida de una nueva línea.
+/* Nombre de la tarea : first_word
+Ficheros esperados : first_word.c
+Funciones permitidas : write
 
-Una palabra es una sección de una cadena delimitada por:
-espacios/tablillas o por el inicio/final de la cadena.
+Escribe un programa que tome una cadena
+y muestre su primera palabra seguida de una nueva línea.
 
-Si el número de parámetros no es 1, o si no hay palabras, simplemente muestra
-una nueva línea.
+Una palabra es una sección de la cadena delimitada por espacios /
+tablillas o por el inicio / fin de la cadena.
+
+Si el número de parámetros no es 1, o si no hay palabras,
+simplemente muestra una nueva línea.
+
+Ejemplos :
+$> ./first_word "FOR PONY" | cat -e
+FOR$
+$> ./first_word "this        ...    is sparta, then again, maybe    not" | cat
+	-e
+this$
+$> ./first_word "   " | cat -e
+$
+$> ./first_word "a" "b" | cat -e
+$
+$> ./first_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+
 */
 
-#include <stdio.h>
 #include <unistd.h>
 
-int	ft_isspace(int c)
-{
-	return (c == 9 || c == 10 || c == 11 || c == 12 || c == 13 || c == 32);
-}
-
-int	main(int ac, char **av)
+int	main(int ag, char **av)
 {
 	int i = 0;
 
-	if (ac == 2)
+	if (ag == 2)
 	{
-		while (ft_isspace(av[1][i]))
-		{
+		while (av[1][i] == 9 || av[1][i] == 32)
 			i++;
-		}
-		while (av[1][i] != '\0' && !ft_isspace(av[1][i]))
-		{
-			write(1, &av[1][i], 1);
-			i++;
-		}
+		while (av[1][i] != 9 && av[1][i] != 32)
+			write(1, &av[1][i++], 1);
 	}
 	write(1, "\n", 1);
-	return (0);
 }
