e0643777f3ae66f69c26b2d4e65e88fba747bfb8
PajaritoCantor <juandrod@ucm.es>
2024-12-06 13:16:50 +0100

    updated

--- exam02/level1/fizzbuzz.c
+++ exam02/level1/fizzbuzz.c
@@ -5,50 +5,76 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/29 02:37:46 by jurodrig          #+#    #+#             */
-/*   Updated: 2024/09/29 03:00:15 by jurodrig         ###   ########.fr       */
+/*   Created: 2024/12/06 12:50:43 by jurodrig          #+#    #+#             */
+/*   Updated: 2024/12/06 13:16:18 by jurodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-Escribe un programa que imprima los números del 1 al 100, 
+Escribe un programa que imprima los números del 1 al 100,
 cada uno separado por una nueva línea.
 
 Si el número es múltiplo de 3, imprime 'fizz' en su lugar.
 
 Si el número es múltiplo de 5, imprime 'buzz' en su lugar.
 
-Si el número es a la vez múltiplo de 3 y múltiplo de 5, 
+Si el número es a la vez múltiplo de 3 y múltiplo de 5,
 imprime 'fizzbuzz' en su lugar.
+
+Example:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$>
+
 */
 
 #include <unistd.h>
 
-void ft_write_number(int n)
+void	write_nbr(int number)
 {
-    char    str[10] = "0123456789";
+	char	str[10] = "123456789";
 
-    if (n > 9)
-        ft_write_number(n / 10);
-    write (1, &str[n % 10], 1);
+	if (number > 9)
+		write_nbr(number / 10);
+	write(1, &str[number % 10], 1);
 }
 
-int main(int argc, int **av)
+int	main(void)
 {
-    int i;
-
-    i = 1;
-    while (i <= 100)
-    {
-        if (i % 15 == 0)
-                write (1, "fizzbuzz", 8);
-        else if (i % 3 == 0)
-                write (1, "fizz", 4);
-        else if (i % 5 == 0)
-                write (1, "buzz", 4);
-        else
-                ft_write_number(i);
-        i++;
-        write (1, "\n", 1);
-    }
+	int i = 0;
+	{
+		while (i <= 100)
+		{
+			if (i % 3 == 0)
+				write(1, "fizz", 4);
+			else if (i % 5 == 0)
+				write(1, "buzz", 4);
+			else if (i % 15 == 0)
+				write(1, "fizzbuzz", 8);
+			else
+				write_nbr(i);
+			i++;
+			write(1, "\n", 1);
+		}
+	}
 }
