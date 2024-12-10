28fbcd657d362f809ac4058390535e067180ef19
PajaritoCantor <juandrod@ucm.es>
2024-12-10 23:59:14 +0100

    dup

---
+++ exam02/level2/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 22:49:51 by jurodrig          #+#    #+#             */
+/*   Updated: 2024/12/10 23:56:28 by jurodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int i = 0;
+	int length = 0;
+	char *strcpy;
+
+	while (src[length])
+		length++;
+	strcpy = malloc(sizeof(*strcpy) * (length + 1));
+	if (strcpy != NULL)
+	{
+		while (src[i])
+		{
+			strcpy = src[i];
+			i++;
+		}
+		strcpy = '\0';
+	}
+	return (strcpy);
+}
