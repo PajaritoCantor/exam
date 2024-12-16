88da9f9a5eaa709629cb22f1241e064e16894685
PajaritoCantor <juandrod@ucm.es>
2024-12-16 15:37:01 +0100

    paint_bits

--- exam02/level2/print_bits.c
+++ exam02/level2/print_bits.c
@@ -6,7 +6,7 @@
 /*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 14:46:46 by jurodrig          #+#    #+#             */
-/*   Updated: 2024/12/16 15:06:32 by jurodrig         ###   ########.fr       */
+/*   Updated: 2024/12/16 15:36:36 by jurodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,24 @@ Example, if you pass 2 to print_bits, it will print "00000010"
 
 void	print_bits(unsigned char octet)
 {
-    unsigned char bit[8] = {
-    ((octet & 0b1000000) >> 7) + 48,
-    ((octet & 0b0100000) >> 6) + 48,
-    ((octt & )
-    };
+	unsigned char bits[8] = {
+		((octet & 0b10000000) >> 7) + 48,
+		((octet & 0b01000000) >> 6) + 48,
+		((octet & 0b00100000) >> 5) + 48,
+		((octet & 0b00010000) >> 4) + 48,
+		((octet & 0b00001000) >> 3) + 48,
+		((octet & 0b00000100) >> 2) + 48,
+		((octet & 0b00000010) >> 1) + 48,
+		((octet & 0b00000001) >> 0) + 48,
+	};
+	write(1, bits, 8);
 }
 /*
 int	main(void)
 {
 	unsigned char	octet;
 
-	octet = 156;
+	octet = 8;
 	print_bits(octet);
 	write(1, "\n", 1);
 	return (0);
