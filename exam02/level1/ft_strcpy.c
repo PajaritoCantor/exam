bc061bcb70f3484a918103d82b6c6afa1edc34f6
PajaritoCantor <juandrod@ucm.es>
2024-12-18 08:08:16 +0100

    i

--- exam02/level1/ft_strcpy.c
+++ exam02/level1/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 16:06:04 by jurodrig          #+#    #+#             */
-/*   Updated: 2024/12/15 19:53:21 by jurodrig         ###   ########.fr       */
+/*   Updated: 2024/12/18 08:08:03 by jurodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,23 @@
 
 char	*ft_strcpy(char *s1, char *s2)
 {
-	char *ptr = s1;
+	int i = 0;
 
-	while (*s2)
+	while (s2[i])
 	{
-		*s1 = *s2;
-		ptr++;
-		s2++;
+		s1[i] = s2[i];
+		i++;
 	}
-	*ptr = '\0';
+	s1[i] = '\0';
 	return (s1);
 }
+/*
+int main()
+{
+	char s1 [10];
+	char *s2 = "hola";
+
+	ft_strcpy(s1, s2);
+	printf("%s", s1);
+}
+*/
