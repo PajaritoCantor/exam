70cbff838e664fc7da43c235829afb078b8f09a1
Juan David Rodriguez Laverde <jurodrig@c1r7s4.42madrid.com>
2025-06-15 23:21:54 +0200

    level2

---
+++ exam02/strings/ft_strspn.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strspn.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/15 21:04:34 by jurodrig          #+#    #+#             */
+/*   Updated: 2025/06/15 22:41:43 by jurodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+
+La función strspn() calcula la longitud (en bytes) del segmento inicial de s, 
+que consta únicamente de bytes de accept.
+
+La función strspn() devuelve el número de bytes del segmento inicial de s, 
+que consta únicamente de bytes de accept.
+*/
+
+#include <stdio.h>
+
+size_t  strspn(const char *s, const char *accept)
+{
+    const char *p = s;
+
+    while (*p)
+    {
+        const char *a = accept;
+        while(*a)
+        {
+            if (*p == *a)
+                break ;
+            a++;
+        }
+        if (!*a)
+            break ;
+        p++;
+    }
+    return(p - s);
+}
+
+int main (int ac, char **av)
+{
+    if (ac == 3)
+    {
+        size_t s = strspn(av[1], av[2]);
+        printf("%zu\n", s);
+    }
+    return 0;
+}
