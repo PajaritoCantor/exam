4348bbbd1397f87641125000c6d524ac93fff575
Juan <juandrod@ucm.es>
2025-06-16 18:04:43 +0200

    strings

---
+++ exam02/strings/wdmatch.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wdmatch.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan <juan@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 17:45:05 by juan              #+#    #+#             */
+/*   Updated: 2025/06/16 18:01:44 by juan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Escribe un programa que tome dos cadenas
+y compruebe si es posible escribir la primera cadena
+con caracteres de la segunda cadena,
+respetando el orden en que estos caracteres aparecen en la segunda cadena.
+
+Si es posible, el programa muestra la cadena, seguida de un \n,
+	de lo contrario simplemente muestra un \n.
+
+Si el número de argumentos no es 2, el programa muestra un \n.
+
+Examples:
+
+$>./wdmatch "faya" "fgvvfdxcacpolhyghbreda" | cat -e
+faya$
+$>./wdmatch "faya" "fgvvfdxcacpolhyghbred" | cat -e
+$
+$>./wdmatch "forty two" "qfqfsoudf arzgrsayns tsryegftdgs sjytwdekuooixq " | cat
+	-e
+forty two$
+$>./wdmatch "error" rrerrrfiiljdfxjyuifrrvcoojh | cat -e
+$
+$>./wdmatch | cat -e
+$
+*/
+
+#include <unistd.h>
+
+int main (int ac, char **av)
+{
+    int i = 0;
+    int j = 0;
+
+    if (ac == 3)
+    {
+        while (av[2][j])
+        {
+            if (av[1][i] == av[2][j])
+                i++;
+            j++;
+        }
+        if (!av[1][i])
+            i = 0;
+        while (av[1][i])
+            write(1, &av[1][i++], 1);
+    }
+    write(1, "\n", 1);
+}
