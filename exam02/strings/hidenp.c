cf019f11e89edee7d813d4b0ffd0d9c1bd70acdb
Juan <juandrod@ucm.es>
2025-06-24 08:30:52 +0200

    hidenp

---
+++ exam02/strings/hidenp.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hidenp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan <juan@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 08:23:43 by juan              #+#    #+#             */
+/*   Updated: 2025/06/24 08:30:38 by juan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Nombre de la asignación: hidenp
+Archivos esperados: hidenp.c
+Funciones permitidas: write
+--------------------------------------------------------------------------------
+
+Escriba un programa llamado hidenp que tome dos cadenas y muestre 1 seguido de un salto de línea si la primera cadena está oculta en la segunda.
+De lo contrario, muestre 0 seguido de un salto de línea.
+
+Sean s1 y s2 cadenas. Decimos que s1 está oculto en s2 si es posible encontrar cada carácter de s1 en s2, en el mismo orden en que aparecen en s1.
+Además, la cadena vacía está oculta en cualquier cadena.
+
+Si el número de parámetros no es 2, el programa muestra un salto de línea.
+
+Ejemplos:
+
+$>./hidenp "fgex.;" "tyf34gdgf;'ektufjhgdgex.;.;rtjynur6" | gato -e
+1$
+$>./hidenp "abc" "2altrb53c.sse" | gato -e
+1$
+$>./hidenp "abc" "btarc" | gato -e
+0$
+$>./hidenp | gato -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int main (int ac, char **av)
+{
+    if (ac == 3)
+    {
+        int i = 0;
+        int j = 0;
+        while (av[1][i] && av[2][j])
+        {
+            if (av[1][i] == av[2][j])
+                i++;
+            j++;
+        }
+        if (!av[1][i])
+            write(1, "1", 1);
+        else
+            write(1, "0", 1);
+    }
+    write(1, "\n", 1);
+    return 0;
+}
