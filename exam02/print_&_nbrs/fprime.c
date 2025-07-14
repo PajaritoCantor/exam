4de89a88debf0391530e0626245483b2aeb1dadb
Juan <juandrod@ucm.es>
2025-07-14 19:19:14 +0200

    updated

--- exam02/print_&_nbrs/fprime.c
+++ exam02/print_&_nbrs/fprime.c
@@ -6,36 +6,37 @@
 /*   By: juan <juan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 11:06:53 by juan              #+#    #+#             */
-/*   Updated: 2025/06/27 17:27:14 by juan             ###   ########.fr       */
+/*   Updated: 2025/07/14 19:19:04 by juan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main (int ac, char **av)
 {
     if (ac == 2)
     {
-        int i = 1;
-        int number = atoi(av[1]);
-
-        if (number == 1)
+        int i = 2;
+        int n = atoi(av[1]);
+        
+        if (n == 1)
             printf("1");
-
-        while (number >= ++i)
+        
+        while (n >= i)
         {
-            if (number % i == 0)
+            if (n % i == 0)
             {
                 printf("%d", i);
-                if (number == i)
+                if (n == i)
                     break ;
                 printf("*");
-                number /= i;
+                n /= i;
                 i = 1;
-
             }
+            i++;
         }
     }
+    printf("\n");
+    return 0;
 }
