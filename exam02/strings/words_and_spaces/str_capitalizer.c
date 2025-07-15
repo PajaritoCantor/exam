893dc2a1d1187a5d2027027c4965c5750c9bfcf9
Juan <juandrod@ucm.es>
2025-07-16 01:23:09 +0200

    capitalizer

--- exam02/strings/words_and_spaces/str_capitalizer.c
+++ exam02/strings/words_and_spaces/str_capitalizer.c
@@ -3,15 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   str_capitalizer.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jurodrig <jurodrig@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juan <juan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
 /*   Created: 2025/06/26 16:23:50 by juan              #+#    #+#             */
-/*   Updated: 2025/07/03 17:44:16 by juan             ###   ########.fr       */
-=======
-/*   Created: 2025/07/12 22:46:04 by jurodrig          #+#    #+#             */
-/*   Updated: 2025/07/12 22:46:57 by jurodrig         ###   ########.fr       */
->>>>>>> 4e2e2b58486711bcaa6f9993dfef52ac1d9394f6
+/*   Updated: 2025/07/16 01:08:56 by juan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,40 +14,23 @@
 
 int main (int ac, char **av)
 {
-<<<<<<< HEAD
-    if (ac > 2)
+    if (ac > 1)
     {
         int j = 0;
         while (av[j])
-=======
-    if (ac > 1)
-    {
-        int j = 1;
-        while(av[j])
->>>>>>> 4e2e2b58486711bcaa6f9993dfef52ac1d9394f6
         {
             int i = 0;
             while (av[j][i])
             {
-<<<<<<< HEAD
                 if (i == 0 || av[j][i - 1] == 32 || av[j][i] == 9)
-=======
-                if (av[j][i] >= 65 && av[j][i] <= 90)
-                        av[j][i] += 32; 
-                if ((i == 0 || av[j][i - 1] == 32 || av[j][i - 1] == 9 || av[j][i - 1] == 13))
->>>>>>> 4e2e2b58486711bcaa6f9993dfef52ac1d9394f6
                 {
                     if (av[j][i] >= 97 && av[j][i] <= 122)
                         av[j][i] -= 32;
                 }
-<<<<<<< HEAD
                 else if (av[j][i] >= 65 && av[j][i] <= 90)
                     av[j][i] += 32;
                 write(1, &av[j][i], 1);
                 i++;
-=======
-                    write(1, &av[j][i++], 1);
->>>>>>> 4e2e2b58486711bcaa6f9993dfef52ac1d9394f6
             }
              if (av[j + 1])
                 write(1, "\n", 1); 
