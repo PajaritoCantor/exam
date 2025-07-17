4aafe40e59874562af2c1e61579a8cbf3e52dce8
Juan <juandrod@ucm.es>
2025-07-17 15:40:11 +0200

    foreach

--- exam02/lists/ft_list_foreach/ft_list_foreach.c
+++ exam02/lists/ft_list_foreach/ft_list_foreach.c
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: juan <juan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/24 07:26:05 by juan              #+#    #+#             */
-/*   Updated: 2025/06/24 07:30:06 by juan             ###   ########.fr       */
+/*   Created: 2025/07/17 14:51:21 by juan              #+#    #+#             */
+/*   Updated: 2025/07/17 15:27:36 by juan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_list.h"
+#include "ft_list_foreach.h"
 
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
     while (begin_list)
     {
-        (*f)(begin_list->data);
-        begin_list = begin_list->next;
+        (*f)(begin_list->data); // aplico la lista a los datos
+        begin_list = begin_list->next; // declaro la lista al siguiente nodo (next)
     }
 }
