ec0b0ff40f36feb6ec2ffdf38f772835499c165c
Pajarito Cantor <juandrod@ucm.es>
2025-07-22 08:55:10 +0200

    easy split

--- exam02/strings/libft/ft_split.c
+++ exam02/strings/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: juan <juan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 21:27:55 by juan              #+#    #+#             */
-/*   Updated: 2025/07/21 22:22:35 by juan             ###   ########.fr       */
+/*   Updated: 2025/07/22 08:50:48 by juan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,52 +35,45 @@ char **ft_split(char *str);
 
 char **ft_split(char *str)
 {
-    int j = 0;
     int k = 0;
+    int j = 0;
     char **split = (char **)malloc(sizeof(char *) * 256);
+
     if (!split)
         return NULL;
-
     while (*str == 32 || *str == 9 || *str == 10)
         str++;
     while (*str)
     {
         j = 0;
-        split[k] = (char *)malloc(sizeof(char) * 4096);
+        split[k] = (char *)malloc(sizeof(char *) * 4096);
 
-        while (*str && *str != 32 && *str != 9 && *str != 10)
+        while (*str && *str != 32 && *str != 9 && *str != 9)
             split[k][j++] = *str++;
 
         split[k][j] = '\0';
         k++;
 
-        while (*str == 32 || *str == 9 || *str == 10)
+        while (*str == 32 || *str == 9 || *str == 9)
             str++;
     }
     split[k] = NULL;
     return (split);
 }
 
-int main() {
-    char str[] = "   Esto es  un    ejemplo   de   split. ";
-    char **result;
-    int i;
+int main ()
+{
+    char str [] = "Este es un ejemplo de split , hola h";
+    int i = 0;
 
-    result = ft_split(str);
+    char **result = ft_split(str);
 
-    if (!result) {
-        fprintf(stderr, "Error al dividir la cadena.\n");
-        return (1);
-    }
-
-    i = 0;
-    while (result[i]) {
-        printf("Palabra %d: '%s'\n", i, result[i]);
-        free(result[i]); // Libera cada palabra
+    while (result[i])
+    {
+        printf("%s\n", result[i]);
+        free(result[i]);
         i++;
     }
-
-    free(result); // Libera el array de punteros
-
+    free(result);
     return (0);
 }
