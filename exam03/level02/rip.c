b08dfb25d8b6b81b307a7dc9e8629d8123bdbe21
Juancho <juandrod@ucm.es>
2025-12-21 23:03:45 +0100

    rip

--- exam03/level02/rip.c
+++ exam03/level02/rip.c
@@ -1,17 +1,23 @@
+/*
 Nombre de la tarea: rip
 Archivos esperados: *.c, *.h
 
 Funciones permitidas: puts, write
 
-Descripción: Escribe un programa que reciba como argumento una cadena que contenga únicamente paréntesis.
+Descripción: Escribe un programa que reciba como argumento 
+una cadena que contenga únicamente paréntesis.
 
-Si los paréntesis están desbalanceados (por ejemplo, "())"), tu programa deberá eliminar el número mínimo de paréntesis para que la expresión sea válida (balanceada).
+Si los paréntesis están desbalanceados (por ejemplo, "())"), 
+tu programa deberá eliminar el número mínimo de paréntesis 
+para que la expresión sea válida (balanceada).
 
 Por "eliminar", nos referimos a sustituir el carácter por un espacio.
 
-Debes imprimir todas las soluciones posibles (puede haber más de una). El orden en que se impriman las soluciones no es importante.
+Debes imprimir todas las soluciones posibles (puede haber más de una). 
+El orden en que se impriman las soluciones no es importante.
 
-Ejemplos de funcionamiento: (Por razones de legibilidad, en los ejemplos el guion bajo _ representa un espacio).
+Ejemplos de funcionamiento: (Por razones de legibilidad, 
+en los ejemplos el guion bajo _ representa un espacio).
 
 ./rip '( ( )'
 
@@ -31,19 +37,16 @@ _ ( )
 
 ( _ ( ) ) ( )
 
-
+*/
 #include <stdio.h>
 
 // 1. Función de validación (Se mantiene igual, ignora espacios automáticamente)
-int is_val(char *str)
-{
+int is_val(char *str){
     int opened = 0, closed = 0;
-    for (int i = 0; str[i]; i++)
-    {
+    for (int i = 0; str[i]; i++){
         if (str[i] == '(')
             opened++;
-        else if (str[i] == ')')
-        {
+        else if (str[i] == ')'){
             if (opened > 0)
                 opened--;
             else
@@ -53,50 +56,32 @@ int is_val(char *str)
     return opened + closed;
 }
 
-// 2. Función RIP arreglada
-void rip(char *str, int must_fix, int n_fix, int pos)
-{
-    // CASO BASE: Si ya hemos puesto exactamente el número de espacios necesarios
-    if (n_fix == must_fix)
-    {
+void rip(char *str, int must_fix, int n_fix, int pos){
+    if (n_fix == must_fix){
         if (is_val(str) == 0)
             puts(str);
         return;
     }
-
-    // El bucle busca el PRÓXIMO paréntesis para convertirlo en espacio
-    for (int i = pos; str[i]; i++)
-    {
-        if (str[i] == '(' || str[i] == ')')
-        {
+    for (int i = pos; str[i]; i++){
+        if (str[i] == '(' || str[i] == ')'){
             char c = str[i];
-            str[i] = ' '; // "Eliminamos"
-            
-            // EL CAMBIO CLAVE: i + 1
-            // Pasamos i + 1 para que la siguiente llamada empiece a buscar 
-            // DESPUÉS de donde acabamos de poner el espacio.
-            rip(str, must_fix, n_fix + 1, i + 1);
-            
-            str[i] = c; // Backtracking: restauramos el paréntesis
+            str[i] = ' ';
+            rip(str, must_fix, n_fix + 1, i + 1);           
+            str[i] = c;
         }
     }
 }
 
 int main(int ac, char **ag)
 {
-    // Validación de entrada para evitar Segfaults
     if (ac != 2 || !ag[1][0])
         return (1);
-
     int m_fix = is_val(ag[1]);
-    
-    // Si ya es válido, se imprime una vez y terminamos
     if (m_fix == 0)
     {
         puts(ag[1]);
         return (0);
     }
-    
     rip(ag[1], m_fix, 0, 0);
     return (0);
 }
